4c02c3956d1dbb0391edca55b6d6acabf54e1927
LP (BB) <lee.prosser@embersoft.co.uk>
2018-11-07 13:17:20 +0000

    Bumped build

--- build/pack.bat
+++ build/pack.bat
@@ -1,5 +1,6 @@
-set version=0.1.0
+set version=0.1.1
 dotnet pack ../src/OpenRpg.Core -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/OpenRpg.Items -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/OpenRpg.Localization -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/OpenRpg.Data -c Release -o ../../_dist /p:version=%version%
+dotnet pack ../src/OpenRpg.Genres.Fantasy -c Release -o ../../_dist /p:version=%version%
