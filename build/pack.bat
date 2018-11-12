8d75a574a8eaea8cabf845a7742689de1cef7e6e
LP <grofit1@yahoo.com>
2018-11-12 20:12:30 +0000

    Bumped version

--- build/pack.bat
+++ build/pack.bat
@@ -1,6 +1,7 @@
-set version=0.2.0
+set version=0.3.0
 dotnet pack ../src/OpenRpg.Core -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/OpenRpg.Items -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/OpenRpg.Localization -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/OpenRpg.Data -c Release -o ../../_dist /p:version=%version%
+dotnet pack ../src/OpenRpg.Quests -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/OpenRpg.Genres.Fantasy -c Release -o ../../_dist /p:version=%version%
