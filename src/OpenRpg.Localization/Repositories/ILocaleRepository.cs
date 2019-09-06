be8be0c5bccbcc193dab020f906e7a4bdaa281f1
LP <lee.prosser@embersoft.co.uk>
2019-09-06 12:36:13 +0100

    Added missing retrieve method to Locale Repo

--- src/OpenRpg.Localization/Repositories/ILocaleRepository.cs
+++ src/OpenRpg.Localization/Repositories/ILocaleRepository.cs
@@ -6,6 +6,7 @@ namespace OpenRpg.Localization.Repositories
     {
         string LocaleCode { get; }
 
+        string Retrieve(string id);
         bool Has(string id);
         void Create(string id, string text);
         void Update(string id, string text);
