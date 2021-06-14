7baed44d18dd12f5313ba6c4e1d78a72df4867f7
LP <grofit1@yahoo.com>
2021-06-14 10:23:41 +0100

    Updated extension method to have correct behaviour
    The check all requirements failed if there was no requirements so this has been fixes.

--- src/OpenRpg.Core/Extensions/IRequirementCheckerExtensions.cs
+++ src/OpenRpg.Core/Extensions/IRequirementCheckerExtensions.cs
@@ -7,6 +7,9 @@ namespace OpenRpg.Core.Extensions
     public static class IRequirementCheckerExtensions
     {
         public static bool AreRequirementsMet<T>(this IRequirementChecker<T> checker, T target, IEnumerable<Requirement> requirements)
-        { return requirements.All(x => checker.IsRequirementMet(target, x)); }
+        { return AreRequirementsMet(checker, target, requirements.ToArray()); }
+        
+        public static bool AreRequirementsMet<T>(this IRequirementChecker<T> checker, T target, IReadOnlyCollection<Requirement> requirements)
+        { return requirements.Count == 0 || requirements.All(x => checker.IsRequirementMet(target, x)); }
     }
 }
