11e81c58d1952758fc2660fe3f1d6d7a68b43873
LP <grofit1@yahoo.com>
2021-06-23 11:08:56 +0100

    Moved presets over to expose specific types
    Have also added some more extensions to more easily alter existing presets

--- src/OpenRpg.CurveFunctions/Extensions/ICurveFunctionExtensions.cs
+++ src/OpenRpg.CurveFunctions/Extensions/ICurveFunctionExtensions.cs
@@ -1,3 +1,5 @@
+using OpenRpg.CurveFunctions.Curves;
+
 namespace OpenRpg.CurveFunctions.Extensions
 {
     public static class ICurveFunctionExtensions
@@ -24,6 +26,63 @@ namespace OpenRpg.CurveFunctions.Extensions
             var normalizedValue = value / maxValue;
             var normalizedOutput = curve.Plot(normalizedValue);
             return normalizedOutput * maxValue;
+        }
+
+        public static PolynomialCurveFunction Alter(this PolynomialCurveFunction curve, float? slope = null, float? xShift = null, float? yShift = null, float? exponent = null)
+        {
+            return new PolynomialCurveFunction(
+                slope.HasValue ? slope.Value : curve.Slope,
+                xShift.HasValue ? xShift.Value : curve.XShift,
+                yShift.HasValue ? yShift.Value : curve.YShift,
+                exponent.HasValue ? exponent.Value : curve.Exponent
+            );
+        }
+        
+        public static LogisticCurveFunction Alter(this LogisticCurveFunction curve, float? slope = null, float? xShift = null, float? yShift = null, float? verticalSize = null)
+        {
+            return new LogisticCurveFunction(
+                slope.HasValue ? slope.Value : curve.Slope,
+                xShift.HasValue ? xShift.Value : curve.XShift,
+                yShift.HasValue ? yShift.Value : curve.YShift,
+                verticalSize.HasValue ? verticalSize.Value : curve.VerticalSize
+            );
+        }
+        
+        public static NormalCurveFunction Alter(this NormalCurveFunction curve, float? slope = null, float? xShift = null, float? yShift = null, float? exponent = null)
+        {
+            return new NormalCurveFunction(
+                slope.HasValue ? slope.Value : curve.Slope,
+                xShift.HasValue ? xShift.Value : curve.XShift,
+                yShift.HasValue ? yShift.Value : curve.YShift,
+                exponent.HasValue ? exponent.Value : curve.Exponent
+            );
+        }
+        
+        public static LogitCurveFunction Alter(this LogitCurveFunction curve, float? slope = null, float? xShift = null, float? yShift = null)
+        {
+            return new LogitCurveFunction(
+                slope.HasValue ? slope.Value : curve.Slope,
+                xShift.HasValue ? xShift.Value : curve.XShift,
+                yShift.HasValue ? yShift.Value : curve.YShift
+            );
+        }
+        
+        public static SineCurveFunction Alter(this SineCurveFunction curve, float? slope = null, float? xShift = null, float? yShift = null)
+        {
+            return new SineCurveFunction(
+                slope.HasValue ? slope.Value : curve.Slope,
+                xShift.HasValue ? xShift.Value : curve.XShift,
+                yShift.HasValue ? yShift.Value : curve.YShift
+            );
+        }
+        
+        public static StepCurveFunction Alter(this StepCurveFunction curve, float? stepValue = null, float? minValue = null, float? maxValue = null)
+        {
+            return new StepCurveFunction(
+                stepValue.HasValue ? stepValue.Value : curve.StepValue,
+                minValue.HasValue ? minValue.Value : curve.MinValue,
+                maxValue.HasValue ? maxValue.Value : curve.MaxValue
+            );
         }
     }
 }
--- src/OpenRpg.CurveFunctions/PresetCurves.cs
+++ src/OpenRpg.CurveFunctions/PresetCurves.cs
@@ -4,25 +4,25 @@ namespace OpenRpg.CurveFunctions
 {
     public class PresetCurves
     {
-        public static ICurveFunction Constant = new PolynomialCurveFunction(0f, 0, 0.5f, 0);
-        public static ICurveFunction Linear = new PolynomialCurveFunction(1.0f, 0, 0, 1.0f);
-        public static ICurveFunction InverseLinear = new PolynomialCurveFunction(-1.0f, 1.0f, 0, 1.0f);
-        public static ICurveFunction StandardCooldown = new PolynomialCurveFunction(1.0f, 0f, 0, 6.0f);
-        public static ICurveFunction StandardRuntime = new PolynomialCurveFunction(-1.0f, 0f, 1.0f, 6.0f);
-        public static ICurveFunction QuadraticLowerLeft = new PolynomialCurveFunction(1.0f, 1.0f, 0.0f, 4.0f);
-        public static ICurveFunction QuadraticLowerRight = new PolynomialCurveFunction(1.0f, 0.0f, 0.0f, 4.0f);
-        public static ICurveFunction QuadraticUpperLeft = new PolynomialCurveFunction(-1.0f, 1.0f, 1.0f, 4.0f);
-        public static ICurveFunction QuadraticUpperRight = new PolynomialCurveFunction(-1.0f, 0f, 1.0f, 4.0f);
-        public static ICurveFunction Logistic = new LogisticCurveFunction(1.0f, 0f, 0.0f, 1.0f);
-        public static ICurveFunction InverseLogistic = new LogisticCurveFunction(-1.0f, 0f, 1.0f, 1.0f);
-        public static ICurveFunction Logit = new LogitCurveFunction(1.0f, 0, 0);
-        public static ICurveFunction InverseLogit = new LogitCurveFunction(-1.0f, 0, 0);
-        public static ICurveFunction BellCurve = new NormalCurveFunction(1.0f, 0, 0, 1.0f);
-        public static ICurveFunction InverseBellCurve = new NormalCurveFunction(-1.0f, 0, 1.0f, 1.0f);
-        public static ICurveFunction SineWave = new SineCurveFunction(1.0f, 0, 0);
-        public static ICurveFunction InverseSineWave = new SineCurveFunction(-1.0f, 0, 0);
-        public static ICurveFunction GreaterThanHalf = new StepCurveFunction(0.5f);
-        public static ICurveFunction LessThanHalf = new StepCurveFunction(0.5f, 1.0f, 0.0f);
-        public static ICurveFunction PassThrough = new PassThroughlCurveFunction();
+        public static PolynomialCurveFunction Constant = new PolynomialCurveFunction(0f, 0, 0.5f, 0);
+        public static PolynomialCurveFunction Linear = new PolynomialCurveFunction(1.0f, 0, 0, 1.0f);
+        public static PolynomialCurveFunction InverseLinear = new PolynomialCurveFunction(-1.0f, 1.0f, 0, 1.0f);
+        public static PolynomialCurveFunction StandardCooldown = new PolynomialCurveFunction(1.0f, 0f, 0, 6.0f);
+        public static PolynomialCurveFunction StandardRuntime = new PolynomialCurveFunction(-1.0f, 0f, 1.0f, 6.0f);
+        public static PolynomialCurveFunction QuadraticLowerLeft = new PolynomialCurveFunction(1.0f, 1.0f, 0.0f, 4.0f);
+        public static PolynomialCurveFunction QuadraticLowerRight = new PolynomialCurveFunction(1.0f, 0.0f, 0.0f, 4.0f);
+        public static PolynomialCurveFunction QuadraticUpperLeft = new PolynomialCurveFunction(-1.0f, 1.0f, 1.0f, 4.0f);
+        public static PolynomialCurveFunction QuadraticUpperRight = new PolynomialCurveFunction(-1.0f, 0f, 1.0f, 4.0f);
+        public static LogisticCurveFunction Logistic = new LogisticCurveFunction(1.0f, 0f, 0.0f, 1.0f);
+        public static LogisticCurveFunction InverseLogistic = new LogisticCurveFunction(-1.0f, 0f, 1.0f, 1.0f);
+        public static LogitCurveFunction Logit = new LogitCurveFunction(1.0f, 0, 0);
+        public static LogitCurveFunction InverseLogit = new LogitCurveFunction(-1.0f, 0, 0);
+        public static NormalCurveFunction BellCurve = new NormalCurveFunction(1.0f, 0, 0, 1.0f);
+        public static NormalCurveFunction InverseBellCurve = new NormalCurveFunction(-1.0f, 0, 1.0f, 1.0f);
+        public static SineCurveFunction SineWave = new SineCurveFunction(1.0f, 0, 0);
+        public static SineCurveFunction InverseSineWave = new SineCurveFunction(-1.0f, 0, 0);
+        public static StepCurveFunction GreaterThanHalf = new StepCurveFunction(0.5f);
+        public static StepCurveFunction LessThanHalf = new StepCurveFunction(0.5f, 1.0f, 0.0f);
+        public static PassThroughlCurveFunction PassThrough = new PassThroughlCurveFunction();
     }
 }
