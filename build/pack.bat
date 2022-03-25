90dc1411482447ed919a4a8772ac7a5b144d2187
LP <grofit1@yahoo.com>
2022-03-25 10:48:11 +0000

    Registered missing nuget packages

--- build/pack.bat
+++ build/pack.bat
@@ -1,9 +1,11 @@
-set version=0.7.20
+set version=0.19.00
 dotnet pack ../src/OpenRpg.Core -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/OpenRpg.Items -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/OpenRpg.Combat -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/OpenRpg.Localization -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/OpenRpg.Data -c Release -o ../../_dist /p:version=%version%
+dotnet pack ../src/OpenRpg.Data.InMemory -c Release -o ../../_dist /p:version=%version%
+dotnet pack ../src/OpenRpg.Data.Database -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/OpenRpg.Quests -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/OpenRpg.Genres.Fantasy -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/OpenRpg.Cards -c Release -o ../../_dist /p:version=%version%
