c416b555281b5f045fe1b8f6be4416e260b7dce1
LP <grofit1@yahoo.com>
2022-06-21 12:08:01 +0100

    Updating the populator to allow repopulation
    In some cases you may not want to change the top level reference and just reuse the existing data.

--- src/OpenRpg.Core/Variables/IVariablePopulator.cs
+++ src/OpenRpg.Core/Variables/IVariablePopulator.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
 using OpenRpg.Core.Effects;
+using OpenRpg.Core.Stats.Variables;
 
 namespace OpenRpg.Core.Variables
 {
-    public interface IVariablePopulator<out T> where T : IVariables
+    public interface IVariablePopulator<T> where T : IVariables
     {
         T Populate(IReadOnlyCollection<Effect> activeEffects, IReadOnlyCollection<IVariables> variables);
+        void Repopulate(T existingVars, IReadOnlyCollection<Effect> activeEffects, IReadOnlyCollection<IVariables> variables);
     }
 }
