9e19d1889b45f6b5ca9d8d2050ede12f0ef73f4e
LP <grofit1@yahoo.com>
2025-01-24 16:30:53 +0000

    Updated stylings and moved to a shared asset component

--- src/OpenRpg.Editor.Core/Extensions/LoadedProjectExtensions.cs
+++ src/OpenRpg.Editor.Core/Extensions/LoadedProjectExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using OpenRpg.Editor.Core.Models;
 
@@ -31,4 +32,14 @@ public static class LoadedProjectExtensions
     
     public static string GetAssetPath(this LoadedProject project, string type, string assetCode, string extension)
     { return $"{GetAssetPath(project, type)}/{assetCode}.{extension}"; }
+
+    public static string GetImageAssetAsDataUrl(this LoadedProject project, string type, string assetCode, string extension)
+    {
+        var imagePath = GetAssetPath(project, type, assetCode, extension);
+        if(!File.Exists(imagePath)) { return string.Empty; }
+        
+        var binaryData = File.ReadAllBytes(imagePath);
+        var base64String = Convert.ToBase64String(binaryData);
+        return $"data:image/png;base64,{base64String}";
+    }
 }
