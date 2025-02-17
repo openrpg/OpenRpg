aca327fdd5fd8a4b7d34c5b0390fdd63843fe238
LP <grofit1@yahoo.com>
2025-02-17 17:52:34 +0000

    Adding basic support for scaled effects to editor

--- src/OpenRpg.Entities/Effects/IEffect.cs
+++ src/OpenRpg.Entities/Effects/IEffect.cs
@@ -1,15 +1,21 @@
+using System.Collections.Generic;
 using OpenRpg.Entities.Requirements;
 
 namespace OpenRpg.Entities.Effects
 {
     /// <summary>
-    /// Generic effect interface that lets us all agree every effect has a type
+    /// Generic effect interface that lets us all agree every effect has a type and requirements
     /// </summary>
     public interface IEffect : IHasRequirements
     {
         /// <summary>
         /// The effect type to apply
         /// </summary>
-        public int EffectType { get; }
+        int EffectType { get; set; }
+        
+        /// <summary>
+        /// Requirements required for this object to function/be used
+        /// </summary>
+        new IReadOnlyCollection<Requirement> Requirements { get; set; }
     }
 }
