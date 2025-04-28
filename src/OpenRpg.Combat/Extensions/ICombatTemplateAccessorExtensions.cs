205143a97eefde555e162e3aa4aaa9a89133421b
LP <grofit1@yahoo.com>
2025-04-28 11:18:31 +0100

    Added missing extension method

--- src/OpenRpg.Combat/Extensions/ICombatTemplateAccessorExtensions.cs
+++ src/OpenRpg.Combat/Extensions/ICombatTemplateAccessorExtensions.cs
@@ -7,5 +7,11 @@ namespace OpenRpg.Combat.Extensions
     {
         public static AbilityTemplate GetAbilityTemplate(this ITemplateAccessor templateAccessor, int abilityTemplateId)
         { return templateAccessor.Get<AbilityTemplate>(abilityTemplateId); }
+        
+        public static Ability ToInstance(this ITemplateAccessor templateAccessor, AbilityData abilityData)
+        {
+            var template = templateAccessor.Get<AbilityTemplate>(abilityData.TemplateId);
+            return new Ability() { Data = abilityData, Template = template };
+        }
     }
 }
