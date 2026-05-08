9644c02e1313fbdd84b09242d7344227379941fd
LP <grofit1@yahoo.com>
2026-05-08 12:55:12 +0100

    Minor updates

--- src/OpenRpg.Editor.Core/Models/Project.cs
+++ src/OpenRpg.Editor.Core/Models/Project.cs
@@ -4,7 +4,7 @@ namespace OpenRpg.Editor.Core.Models;
 
 public class Project
 {
-    public string Version { get; set; } = "1.0.0";
+    public string Version { get; set; } = "1.0.1";
     public string AssetsFolder { get; set; } = "assets";
     public string TemplatesFolder { get; set; } = "templates";
     public string LocalesFolder { get; set; } = "locales";
