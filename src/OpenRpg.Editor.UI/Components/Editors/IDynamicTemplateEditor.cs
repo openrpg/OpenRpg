bf151d7d0ea5c9bd1e74952c4fe5afcc5fd48b20
LP <grofit1@yahoo.com>
2026-05-12 08:42:21 +0100

    More updates to template

---
+++ src/OpenRpg.Editor.UI/Components/Editors/IDynamicTemplateEditor.cs
@@ -0,0 +1,23 @@
+namespace OpenRpg.Editor.UI.Components.Editors;
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+public interface IDynamicTemplateEditor
+{
+    object Template { get; set; }
+    string AssetCodePrefix { get; set; }
+    IReadOnlyList<TemplateEditorSection> Sections { get; set; }
+    
+    RenderFragment PrimaryDetailsContent { get; set; }
+    RenderFragment SecondaryDetailsContent { get; set; }
+}
+
+public class TemplateEditorSection
+{
+    public string Title { get; set; }
+    public string Property { get; set; }
+    public string EditorType { get; set; }
+    public string EditorComponent { get; set; }
+    public Dictionary<string, string> Options { get; set; } = new();
+}
