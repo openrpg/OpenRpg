3f8a5de126f46d210850852ed9a671d76f27da34
LP <grofit1@yahoo.com>
2026-05-13 09:12:45 +0100

    Made sure the genre types are combined and output plugins in debug mode

--- src/OpenRpg.Editor.Infrastructure/Plugins/CombinedGenreTypesProvider.cs
+++ src/OpenRpg.Editor.Infrastructure/Plugins/CombinedGenreTypesProvider.cs
@@ -9,18 +9,18 @@ public class CombinedGenreTypesProvider : IGenreTypesProvider
 {
     public string PluginId => "combined";
 
-    public OptionData[] ItemTypes => _providers.SelectMany(p => p.ItemTypes).ToArray();
-    public OptionData[] ItemQualityTypes => _providers.SelectMany(p => p.ItemQualityTypes).ToArray();
-    public OptionData[] RequirementTypes => _providers.SelectMany(p => p.RequirementTypes).ToArray();
-    public OptionData[] EffectTypes => _providers.SelectMany(p => p.EffectTypes).ToArray();
-    public OptionData[] RewardTypes => _providers.SelectMany(p => p.RewardTypes).ToArray();
-    public OptionData[] ModificationTypes => _providers.SelectMany(p => p.ModificationTypes).ToArray();
-    public OptionData[] ObjectiveTypes => _providers.SelectMany(p => p.ObjectiveTypes).ToArray();
-    public OptionData[] EffectScalingType => _providers.SelectMany(p => p.EffectScalingType).ToArray();
-    public OptionData[] StatTypes => _providers.SelectMany(p => p.StatTypes).ToArray();
-    public OptionData[] StateTypes => _providers.SelectMany(p => p.StateTypes).ToArray();
-    public OptionData[] CraftingSkillTypes => _providers.SelectMany(p => p.CraftingSkillTypes).ToArray();
-    public OptionData[] GatheringSkillTypes => _providers.SelectMany(p => p.GatheringSkillTypes).ToArray();
+    public OptionData[] ItemTypes => _providers.SelectMany(p => p.ItemTypes).GroupBy(o => o.Id).Select(g => g.First()).ToArray();
+    public OptionData[] ItemQualityTypes => _providers.SelectMany(p => p.ItemQualityTypes).GroupBy(o => o.Id).Select(g => g.First()).ToArray();
+    public OptionData[] RequirementTypes => _providers.SelectMany(p => p.RequirementTypes).GroupBy(o => o.Id).Select(g => g.First()).ToArray();
+    public OptionData[] EffectTypes => _providers.SelectMany(p => p.EffectTypes).GroupBy(o => o.Id).Select(g => g.First()).ToArray();
+    public OptionData[] RewardTypes => _providers.SelectMany(p => p.RewardTypes).GroupBy(o => o.Id).Select(g => g.First()).ToArray();
+    public OptionData[] ModificationTypes => _providers.SelectMany(p => p.ModificationTypes).GroupBy(o => o.Id).Select(g => g.First()).ToArray();
+    public OptionData[] ObjectiveTypes => _providers.SelectMany(p => p.ObjectiveTypes).GroupBy(o => o.Id).Select(g => g.First()).ToArray();
+    public OptionData[] EffectScalingType => _providers.SelectMany(p => p.EffectScalingType).GroupBy(o => o.Id).Select(g => g.First()).ToArray();
+    public OptionData[] StatTypes => _providers.SelectMany(p => p.StatTypes).GroupBy(o => o.Id).Select(g => g.First()).ToArray();
+    public OptionData[] StateTypes => _providers.SelectMany(p => p.StateTypes).GroupBy(o => o.Id).Select(g => g.First()).ToArray();
+    public OptionData[] CraftingSkillTypes => _providers.SelectMany(p => p.CraftingSkillTypes).GroupBy(o => o.Id).Select(g => g.First()).ToArray();
+    public OptionData[] GatheringSkillTypes => _providers.SelectMany(p => p.GatheringSkillTypes).GroupBy(o => o.Id).Select(g => g.First()).ToArray();
 
     private readonly List<IGenreTypesProvider> _providers;
 
