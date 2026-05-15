7eba472a0989c9d37b3a4f7e35b4972dbb92729a
LP <grofit1@yahoo.com>
2026-05-15 12:44:00 +0100

    Updated cloning to correctly account for variables

--- src/OpenRpg.Editor.Infrastructure/Services/Cloner.cs
+++ src/OpenRpg.Editor.Infrastructure/Services/Cloner.cs
@@ -1,14 +1,29 @@
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using OpenRpg.Projects.Json.Convertors;
 
 namespace OpenRpg.Editor.Infrastructure.Services
 {
     public class Cloner : ICloner
     {
+        public JsonSerializerSettings SerializationSettings { get; } = new() { 
+            TypeNameHandling = TypeNameHandling.Auto, 
+            Converters = [new VariablesConverter()],
+            MetadataPropertyHandling = MetadataPropertyHandling.ReadAhead
+        };
+        
         public T Clone<T>(T source) where T : new()
         {
-            var serializationSettings = new JsonSerializerSettings() { TypeNameHandling = TypeNameHandling.Auto };
-            var data = JsonConvert.SerializeObject(source, serializationSettings);
-            return JsonConvert.DeserializeObject<T>(data, serializationSettings);
+            var data = JsonConvert.SerializeObject(source, SerializationSettings);
+            return JsonConvert.DeserializeObject<T>(data, SerializationSettings);
+        }
+        
+        public object Clone(object source)
+        {
+            var type = source.GetType();
+            var data = JsonConvert.SerializeObject(source, SerializationSettings);
+            var jObject = JsonConvert.DeserializeObject<JObject>(data, SerializationSettings);
+            return jObject.ToObject(type, JsonSerializer.Create(SerializationSettings));
         }
     }
 }
