a4aa4cfc7ea4632c528017838268e572c9ec9ce1
LP <grofit1@yahoo.com>
2026-05-24 10:59:04 +0100

    Added more tests for abilities and new life restore/resurrection code

--- src/OpenRpg.UnitTests/Genres/Extensions/EntityStateVariableExtensionTests.cs
+++ src/OpenRpg.UnitTests/Genres/Extensions/EntityStateVariableExtensionTests.cs
@@ -3,6 +3,7 @@ using OpenRpg.Combat.Attacks;
 using OpenRpg.Combat.Processors.Attacks;
 using OpenRpg.Entities.State.Variables;
 using OpenRpg.Genres.Extensions;
+using OpenRpg.Genres.Fantasy.Extensions;
 using OpenRpg.Genres.Types;
 using Xunit;
 
@@ -76,7 +77,179 @@ namespace OpenRpg.UnitTests.Genres.Extensions
             var actualHealth = entityState.Health;
             Assert.Equal(0, actualHealth);
         }
-        
+
+        [Fact]
+        public void should_identify_is_dead()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.Health = 0;
+            Assert.True(entityState.IsDead);
+        }
+
+        [Fact]
+        public void should_identify_is_not_dead()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.Health = 1;
+            Assert.False(entityState.IsDead);
+        }
+
+        [Fact]
+        public void should_restore_life()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.RestoreLife(50);
+            Assert.Equal(50, entityState.Health);
+        }
+
+        [Fact]
+        public void should_restore_life_with_maxhealth()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.RestoreLife(80, 100);
+            Assert.Equal(80, entityState.Health);
+        }
+
+        [Fact]
+        public void should_restore_life_with_maxhealth_clamped()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.RestoreLife(120, 100);
+            Assert.Equal(100, entityState.Health);
+        }
+
+        [Fact]
+        public void should_restore_life_with_minimum_to_one()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.RestoreLife(0);
+            Assert.Equal(1, entityState.Health);
+        }
+
+        [Fact]
+        public void should_apply_stamina_changes()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.Stamina = 75;
+            Assert.Equal(75, entityState.Stamina);
+        }
+
+        [Fact]
+        public void should_add_stamina()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.Stamina = 100;
+            entityState.AddStamina(23);
+            Assert.Equal(123, entityState.Stamina);
+        }
+
+        [Fact]
+        public void should_deduct_stamina()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.Stamina = 123;
+            entityState.DeductStamina(23);
+            Assert.Equal(100, entityState.Stamina);
+        }
+
+        [Fact]
+        public void should_add_stamina_with_maxstamina()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.Stamina = 50;
+            entityState.AddStamina(30, 100);
+            Assert.Equal(80, entityState.Stamina);
+        }
+
+        [Fact]
+        public void should_add_stamina_with_maxstamina_clamped()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.Stamina = 85;
+            entityState.AddStamina(30, 100);
+            Assert.Equal(100, entityState.Stamina);
+        }
+
+        [Fact]
+        public void should_deduct_stamina_with_maxstamina()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.Stamina = 80;
+            entityState.DeductStamina(30, 100);
+            Assert.Equal(50, entityState.Stamina);
+        }
+
+        [Fact]
+        public void should_deduct_stamina_with_maxstamina_clamped()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.Stamina = 20;
+            entityState.DeductStamina(50, 100);
+            Assert.Equal(0, entityState.Stamina);
+        }
+
+        [Fact]
+        public void should_apply_mana_changes()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.Mana = 75;
+            Assert.Equal(75f, entityState.Mana);
+        }
+
+        [Fact]
+        public void should_add_mana()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.Mana = 100;
+            entityState.AddMana(23);
+            Assert.Equal(123f, entityState.Mana);
+        }
+
+        [Fact]
+        public void should_deduct_mana()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.Mana = 123;
+            entityState.DeductMana(23);
+            Assert.Equal(100f, entityState.Mana);
+        }
+
+        [Fact]
+        public void should_add_mana_with_maxmana()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.Mana = 50;
+            entityState.AddMana(30, 100);
+            Assert.Equal(80f, entityState.Mana);
+        }
+
+        [Fact]
+        public void should_add_mana_with_maxmana_clamped()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.Mana = 85;
+            entityState.AddMana(30, 100);
+            Assert.Equal(100f, entityState.Mana);
+        }
+
+        [Fact]
+        public void should_deduct_mana_with_maxmana()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.Mana = 80;
+            entityState.DeductMana(30, 100);
+            Assert.Equal(50f, entityState.Mana);
+        }
+
+        [Fact]
+        public void should_deduct_mana_with_maxmana_clamped()
+        {
+            var entityState = new EntityStateVariables();
+            entityState.Mana = 20;
+            entityState.DeductMana(50, 100);
+            Assert.Equal(0f, entityState.Mana);
+        }
+
         [Fact]
         public void should_correctly_deduct_health()
         {
